fc0c8edd211a16eb0e3f60f18ebefa749a1e0b68
Wyatt Kaliser <wyattpetrock@gmail.com>
2026-01-12 18:51:41 -0500

    Update index.js

--- template/index.js
+++ template/index.js
@@ -1,1 +1,48 @@
-(function(e,o,n){"use strict";const{FormText:t}=n.Forms;function l(){return React.createElement(t,null,"Hello, world!")}var r={onLoad:function(){o.logger.log("Hello world!")},onUnload:function(){o.logger.log("Goodbye, world.")},settings:l};return e.default=r,Object.defineProperty(e,"__esModule",{value:!0}),e})({},vendetta,vendetta.ui.components);
+import { findByProps } from "@revenge/modules/webpack";
+import { before } from "@revenge/modules/patcher";
+
+const TAG = "[avatar-override]";
+const TARGET_USER_ID = "376407743776686094";
+const OVERRIDE_IMAGE_URL = "https://cdn.donmai.us/sample/a5/f2/__furina_genshin_impact_drawn_by_overlord_overlord80000__sample-a5f2de3aa9623900360f7c867f42519c.jpg";
+
+let UserUtils;
+let unpatch;
+
+export function onLoad() {
+    console.log(`${TAG} loading`);
+
+    try {
+        UserUtils = findByProps("getAvatarURL");
+        console.log(`${TAG} UserUtils found`, UserUtils);
+    } catch (e) {
+        console.error(`${TAG} failed to find UserUtils`, e);
+        return;
+    }
+
+    unpatch = before("getAvatarURL", UserUtils, (args) => {
+        const [user] = args;
+
+        if (!user) {
+            console.log(`${TAG} getAvatarURL called with no user`);
+            return;
+        }
+
+        console.log(`${TAG} avatar request for user`, user.id);
+
+        if (user.id !== TARGET_USER_ID) return;
+
+        console.log(`${TAG} overriding avatar for target user`);
+        return OVERRIDE_IMAGE_URL;
+    });
+
+    console.log(`${TAG} patch applied`);
+}
+
+export function onUnload() {
+    console.log(`${TAG} unloading`);
+
+    if (unpatch) {
+        unpatch();
+        console.log(`${TAG} patch removed`);
+    }
+}
