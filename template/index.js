6124c94cdbedd1a98d1ec96c389b18f541f24f21
Wyatt Kaliser <wyattpetrock@gmail.com>
2026-01-12 18:52:20 -0500

    Update index.js

--- template/index.js
+++ template/index.js
@@ -1,48 +1,59 @@
-import { findByProps } from "@revenge/modules/webpack";
-import { before } from "@revenge/modules/patcher";
+(function (e, o, n) {
+    "use strict";
 
-const TAG = "[avatar-override]";
-const TARGET_USER_ID = "376407743776686094";
-const OVERRIDE_IMAGE_URL = "https://cdn.donmai.us/sample/a5/f2/__furina_genshin_impact_drawn_by_overlord_overlord80000__sample-a5f2de3aa9623900360f7c867f42519c.jpg";
+    const { findByProps } = vendetta.metro;
+    const { before } = vendetta.patcher;
 
-let UserUtils;
-let unpatch;
+    const TAG = "[avatar-override]";
+    const TARGET_USER_ID = "376407743776686094";
+    const OVERRIDE_IMAGE_URL = "https://cdn.donmai.us/sample/a5/f2/__furina_genshin_impact_drawn_by_overlord_overlord80000__sample-a5f2de3aa9623900360f7c867f42519c.jpg";
 
-export function onLoad() {
-    console.log(`${TAG} loading`);
+    let UserUtils;
+    let unpatch;
 
-    try {
-        UserUtils = findByProps("getAvatarURL");
-        console.log(`${TAG} UserUtils found`, UserUtils);
-    } catch (e) {
-        console.error(`${TAG} failed to find UserUtils`, e);
-        return;
-    }
+    const plugin = {
+        onLoad() {
+            o.logger.log(`${TAG} loading`);
 
-    unpatch = before("getAvatarURL", UserUtils, (args) => {
-        const [user] = args;
+            try {
+                UserUtils = findByProps("getAvatarURL");
+                o.logger.log(`${TAG} UserUtils found`);
+            } catch (err) {
+                o.logger.error(`${TAG} failed to find UserUtils`, err);
+                return;
+            }
 
-        if (!user) {
-            console.log(`${TAG} getAvatarURL called with no user`);
-            return;
+            unpatch = before("getAvatarURL", UserUtils, (args) => {
+                const [user] = args;
+
+                if (!user) {
+                    o.logger.log(`${TAG} getAvatarURL called with no user`);
+                    return;
+                }
+
+                o.logger.log(`${TAG} avatar request for user ${user.id}`);
+
+                if (user.id !== TARGET_USER_ID) return;
+
+                o.logger.log(`${TAG} overriding avatar`);
+                return OVERRIDE_IMAGE_URL;
+            });
+
+            o.logger.log(`${TAG} patch applied`);
+        },
+
+        onUnload() {
+            o.logger.log(`${TAG} unloading`);
+
+            if (unpatch) {
+                unpatch();
+                o.logger.log(`${TAG} patch removed`);
+            }
         }
+    };
 
-        console.log(`${TAG} avatar request for user`, user.id);
+    e.default = plugin;
+    Object.defineProperty(e, "__esModule", { value: true });
+    return e;
 
-        if (user.id !== TARGET_USER_ID) return;
-
-        console.log(`${TAG} overriding avatar for target user`);
-        return OVERRIDE_IMAGE_URL;
-    });
-
-    console.log(`${TAG} patch applied`);
-}
-
-export function onUnload() {
-    console.log(`${TAG} unloading`);
-
-    if (unpatch) {
-        unpatch();
-        console.log(`${TAG} patch removed`);
-    }
-}
+})({}, vendetta, vendetta.ui.components);
