a1fa9de902a6f511c260916858400fc704157de7
Wyatt Kaliser <wyattpetrock@gmail.com>
2026-01-12 19:20:12 -0500

    Update index.js

--- plugins/Override-User-Avatars/src/index.js
+++ plugins/Override-User-Avatars/src/index.js
@@ -4,7 +4,7 @@ import { before } from "@revenge/modules/patcher";
 const TARGET_USER_ID = "376407743776686094";
 const OVERRIDE_IMAGE_URL = "https://cdn.donmai.us/sample/a5/f2/__furina_genshin_impact_drawn_by_overlord_overlord80000__sample-a5f2de3aa9623900360f7c867f42519c.jpg";
 
-const UserUtils = findByProps{"getAvatarURL"};
+const UserUtils = findByProps("getAvatarURL");
 
 let unpatch;
 
