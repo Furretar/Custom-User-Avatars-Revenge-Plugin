4d5608c26fd6b0e5c8e8f2de710779bedf3affaf
Wyatt Kaliser <wyattpetrock@gmail.com>
2026-01-17 04:49:16 -0500

    Update index.ts

--- plugins/Override-User-Avatars/src/index.ts
+++ plugins/Override-User-Avatars/src/index.ts
@@ -1,7 +1,26 @@
-const TAG = "[simple-plugin]";
+import {findByProps} from "@revenge/modules/webpack";
+
+const TAG = "[custom-avatars]";
+const target_id = "376407743776686094";
 
 export function onLoad(): void {
     console.log(`${TAG} loaded`);
+
+    const UserStore = findByProps("getUser");
+
+    if (!UserStore) {
+        console.log(`${TAG} userstore not found`);
+        return;
+    }
+
+    const user = UserStore.getUser(target_id);
+
+    if (!user) {
+        console.log(`${TAG} user not found`);
+        return;
+    }
+
+    console.log(`{TAG} username: ${user.username}#${user.discriminator}`);
 }
 
 export function onUnload(): void {
